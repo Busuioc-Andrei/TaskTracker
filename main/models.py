4eb8fc0b0b1f73bc61304995a46e982940b8b1dd
Busuioc-Andrei <andreitudor23@gmail.com>
2023-03-12 19:14:39 +0200

    First model definition approach, doesn't work

--- main/models.py
+++ main/models.py
@@ -6,18 +6,63 @@ from django.contrib.auth.models import User
 
 class BaseModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=500)
+    name = models.CharField(max_length=300)
+    description = models.CharField(max_length=5000)
     created_at = models.DateTimeField(null=True)
-    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='created_by', null=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='%(class)s_created', null=True)
     modified_at = models.DateTimeField(null=True)
-    modified_by = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='modified_by', null=True)
+    modified_by = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='%(class)s_modified', null=True)
 
     class Meta:
         abstract = True
 
 
-class Task(BaseModel):
+class Project(BaseModel):
+    def __str__(self):
+        return self.name
+
+
+class Board(BaseModel):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Column(BaseModel):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Issue(BaseModel):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    column = models.ForeignKey(Column, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        abstract = True
+
+
+class Epic(Issue):
+    def __str__(self):
+        return self.name
+
+
+class UserStory(Issue):
+    parent_issue = models.ForeignKey(Epic, on_delete=models.SET_NULL, null=True)
+    start_date = models.DateTimeField(null=True)
+    end_date = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(Issue):
+    parent_issue = models.ForeignKey(Issue, on_delete=models.SET_NULL, null=True)
     start_date = models.DateTimeField(null=True)
     end_date = models.DateTimeField(null=True)
 
@@ -27,3 +72,8 @@ class Task(BaseModel):
     def get_absolute_url(self):
         from django.urls import reverse
         return reverse('task-detail', kwargs={'pk': self.pk})
+
+
+class Comment(BaseModel):
+    def __str__(self):
+        return self.name
