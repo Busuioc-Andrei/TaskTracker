41faa2ad6210f01633d75ab8083b71cdaf9e793c
Busuioc-Andrei <andreitudor23@gmail.com>
2023-04-22 15:45:28 +0300

    Added tests for IssueForm validations

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,81 @@
 from django.test import TestCase
 
-# Create your tests here.
+from main.forms import IssueForm
+from main.models import Issue, Project
+
+
+class IssueFormTestCase(TestCase):
+    def setUp(self) -> None:
+        project = Project.objects.create(name='test_project')
+        Issue.objects.create(name='test_task', issue_type='task', project=project)
+        Issue.objects.create(name='test_task2', issue_type='task', project=project)
+        Issue.objects.create(name='test_task3', issue_type='task', project=project)
+        Issue.objects.create(name='test_user_story', issue_type='user_story', project=project)
+        Issue.objects.create(name='test_epic', issue_type='epic', project=project)
+
+    def test_issue_cannot_reference_itself(self):
+        task = Issue.objects.get(name='test_task')
+
+        form = IssueForm(instance=task)
+        data = {
+            'parent_issue': task.id
+        }
+        form = IssueForm(instance=task, data={**form.initial, **data})
+
+        self.assertFormError(form, 'parent_issue', "Issue cannot be it's own parent.")
+
+    def test_user_story_cannot_have_non_epic_parent(self):
+        project = Project.objects.get(name='test_project')
+        task = Issue.objects.get(name='test_task')
+        user_story = Issue.objects.get(name='test_user_story')
+        epic = Issue.objects.get(name='test_epic')
+
+        user_story_data = {'name': 'test_user_story1', 'issue_type': 'user_story', 'parent_issue': task.id, 'project': project.id}
+        form = IssueForm(data=user_story_data)
+        self.assertFormError(form, 'parent_issue', "User Stories can only have Epics as parents.")
+
+        user_story_data['parent_issue'] = user_story
+        form = IssueForm(data=user_story_data)
+        self.assertFormError(form, 'parent_issue', "User Stories can only have Epics as parents.")
+
+        user_story_data['parent_issue'] = epic
+        form = IssueForm(data=user_story_data)
+        self.assertTrue(form.is_valid())
+
+    def test_reject_circular_references(self):
+        task = Issue.objects.get(name='test_task')
+        task2 = Issue.objects.get(name='test_task2')
+        task3 = Issue.objects.get(name='test_task3')
+
+        form = IssueForm(instance=task)
+        data = {
+            'parent_issue': task2.id
+        }
+        form = IssueForm(instance=task, data={**form.initial, **data})
+        self.assertTrue(form.is_valid())
+        form.save()
+
+        form = IssueForm(instance=task2)
+        data = {
+            'parent_issue': task3.id
+        }
+        form = IssueForm(instance=task2, data={**form.initial, **data})
+        self.assertTrue(form.is_valid())
+        form.save()
+
+        form = IssueForm(instance=task3)
+        data = {
+            'parent_issue': task.id
+        }
+        form = IssueForm(instance=task3, data={**form.initial, **data})
+        self.assertFormError(form, 'parent_issue', "Circular reference detected.")
+
+    def test_epic_cannot_have_parent(self):
+        """An epic can't have a parent"""
+        project = Project.objects.get(name='test_project')
+        task = Issue.objects.get(name='test_task')
+
+        epic_data = {'name': 'test_epic1', 'issue_type': 'epic', 'parent_issue': task.id, 'project': project.id}
+        form = IssueForm(data=epic_data)
+
+        self.assertFormError(form, 'parent_issue', 'Epics cannot have a parent Issue.')
